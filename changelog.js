490071d7f38de8b0baa447930eb0c5ad21870903
Arcadier <ios@arcadier.com>
2020-02-07 12:41:22 +0800

    Update changelog.js

--- changelog.js
+++ changelog.js
@@ -7,12 +7,24 @@ $(document).ready(function(){
 		});
 	});
 	
-	var header = document.querySelector(".page-header");
-	var postman = "<div class=\"postman-run-button\" data-postman-action=\"collection/import\" data-postman-var-1=\"346b915f84ec00600260\"></div>";
-	var postman_ctn = document.createElement("div");
-	postman_ctn.innerHTML = postman;
-	header.appendChild(postman_ctn);
+	addPostmanButton();
+	changeViewButton():
 	
-	(function (p,o,s,t,m,a,n) {!p[s] && (p[s] = function (){(p[t] || (p[t] = [])).push(arguments);});!o.getElementById(s+t) && o.getElementsByTagName("head")[0].appendChild(((n = o.createElement("script")), (n.id = s+t), (n.async = 1), (n.src = m), n));}
-	(window, document, "_pm", "PostmanRunObject", "https://run.pstmn.io/button.js"));
+	function addPostmanButton(){
+		var header = document.querySelector(".page-header");
+		var postman = "<div class=\"postman-run-button\" data-postman-action=\"collection/import\" data-postman-var-1=\"346b915f84ec00600260\"></div>";
+		var postman_ctn = document.createElement("div");
+		postman_ctn.innerHTML = postman;
+		header.appendChild(postman_ctn);
+
+		(function (p,o,s,t,m,a,n) {!p[s] && (p[s] = function (){(p[t] || (p[t] = [])).push(arguments);});!o.getElementById(s+t) && o.getElementsByTagName("head")[0].appendChild(((n = o.createElement("script")), (n.id = s+t), (n.async = 1), (n.src = m), n));}
+		(window, document, "_pm", "PostmanRunObject", "https://run.pstmn.io/button.js"));
+	}
+	
+	function changeViewButton(){
+		var view_btn = document.querySelector(".page-header > a.btn");
+		view_btn.innerHTML = "View on Postman";
+		view_btn.removeClass("btn");
+		view_btn.addClass("postman-run-button");
+	}
 });
