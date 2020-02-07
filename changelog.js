43b1cd3c4cd69131346022b46e23f8840ddb6a9d
Arcadier <ios@arcadier.com>
2020-02-07 12:07:23 +0800

    Update changelog.js

--- changelog.js
+++ changelog.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-
+	$("#filter").click(function(){
+		var json_url = "https://raw.githubusercontent.com/Arcadier/API-Changelog/master/Postman%20Collections/Arcadier_API_v2.0.json";
+		$.getJSON(json_url, function(result){ 
+			console.log(result.info.name); 
+			console.log(result.item[0].item[0].name); 
+		});
+	});
+	
+	var header = document.querySelector(".page-header");
+	
 });
