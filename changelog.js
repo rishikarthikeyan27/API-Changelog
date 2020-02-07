5c2d5955c1192482e6eaf7eec5255c338ca07eac
Arcadier <ios@arcadier.com>
2020-02-07 12:33:56 +0800

    Update changelog.js

--- changelog.js
+++ changelog.js
@@ -12,4 +12,7 @@ $(document).ready(function(){
 	var postman_ctn = document.createElement("div");
 	postman_ctn.innerHTML = postman;
 	header.appendChild(postman_ctn);
+	
+	(function (p,o,s,t,m,a,n) {!p[s] && (p[s] = function (){(p[t] || (p[t] = [])).push(arguments);});!o.getElementById(s+t) && o.getElementsByTagName("head")[0].appendChild(((n = o.createElement("script")), (n.id = s+t), (n.async = 1), (n.src = m), n));}
+	(window, document, "_pm", "PostmanRunObject", "https://run.pstmn.io/button.js"));
 });
